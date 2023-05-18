21f89477cb1b638311dc49b6561392db5313f6fe
Aya354 <79589334+Aya354@users.noreply.github.com>
2023-05-18 21:29:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -215,7 +215,68 @@ class Board:
 
 ###################################################################################################################
 #                                              game
+class Game:
+    def __init__(self, win):
+        self._init()
+        self.win = win
 
+    def update(self):
+        self.board.draw(self.win)
+        pygame.display.update()
+
+    def _init(self):
+        self.selected = None
+        self.board = Board()
+        self.turn = RED
+        self.valid_moves = {}
+
+    def winner(self):
+        return self.board.winner()
+
+    def reset(self):
+        self._init()
+
+    def select(self, row, column):
+        if self.selected:
+            result = self._move(row, column)
+            if not result:
+                self.selected = None
+                self.select(row, column)
+
+        piece = self.board.get_piece(row, column)
+        if piece != 0 and piece.color == self.turn:
+            self.selected = piece
+            self.valid_moves = self.board.get_valid_moves(piece)
+            return True
+
+        return False
+
+    def _move(self, row, column):
+        piece = self.board.get_piece(row, column)
+        if self.selected and piece == 0 and (row, column) in self.valid_moves:
+            self.board.move(self.selected, row, column)
+            skipped = self.valid_moves[(row, column)]
+            if skipped:
+                self.board.remove(skipped)
+            self.change_turn()
+        else:
+            return False
+
+        return True
+
+    def change_turn(self):
+        self.valid_moves = {}
+        if self.turn == RED:
+            self.turn = WHITE
+        else:
+            self.turn = RED
+
+    def get_board(self):
+        return self.board
+
+    def ai_move(self, board):
+        self.board = board
+        self.change_turn()
 
 
 ######################################################################################################################
